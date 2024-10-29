4ba5f61565f66b18d04406b90683dc4027c6a32d
unknown <you@example.com>
2024-10-29 22:55:12 +0600

    contest added

--- week-1/vjudge-contest/C_Maximum_Coins.cpp
+++ week-1/vjudge-contest/C_Maximum_Coins.cpp
@@ -23,28 +23,4 @@ cout<<mx<<'\n';
     return 0;
 }
 
-// #include <bits/stdc++.h>
-// using namespace std;int main() {
-//     int T;
-//     cin >> T;
-//     while (T--) {
-//         int N, X;
-//         cin >> N >> X; // Store the coins paid in each game (2^1, 2^2, ..., 2^N)
-//         vector<long long> coins;
-//         for (int i = 1; i <= N; i++) {
-//             coins.push_back(1LL << i);  // Equivalent to 2^i
-//         }
 
-//         // Sort in ascending order to get the smallest values first
-//         sort(coins.begin(), coins.end());
-
-//         // Sum the smallest X values
-//         long long maxCoins = 0;
-//         for (int i = 0; i < X; i++) {
-//             maxCoins += coins[i];
-//         }
-
-//         cout << maxCoins << '\n';
-//     }
-//     return 0;
-// }
